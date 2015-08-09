aa8859fad1905c3412e1e17a931baaf6d8646c41
Andrew Voitov <andreivoitau@gmail.com>
2015-08-09 10:24:18 +0300

    Add temporary method to start app without DB

--- app.js
+++ app.js
@@ -7,9 +7,25 @@ var bodyParser = require('body-parser');
 var mongodbUrl = 'mongodb://localhost:27017/homogen';
 
 MongoClient.connect(mongodbUrl, function(err, db){
-    if(err) throw err;
-    initApplication(db);
+    if(err) {
+    	initApplicationWithoutDB();
+    } else {
+    	initApplication(db);
+    }
 });
+
+//temporary method
+function initApplicationWithoutDB() {
+	console.log("...Without DB...")
+    var app = express();
+    app.set('port', process.env.PORT || 1313);
+    app.use(express.static('frontend'));
+    app.use(bodyParser.json());
+    //run application after mongo connection
+    app.listen(app.get('port'), function() {
+        console.log('Homogen server is started on port: ' + app.get('port'));
+    });
+}
 
 function initApplication(db) {
     var app = express();
