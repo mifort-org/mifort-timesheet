b6acd3c7774314f53570dd5f9b0a28bc924b0f8b
Andrew Voitov <andreivoitau@gmail.com>
2015-09-16 19:42:52 +0300

    Rest API: Replace assignments.
    Not tested!!!!

--- backend/user.js
+++ backend/user.js
@@ -34,13 +34,40 @@ exports.restGetByProjectId = function(req, res) {
         var users = dbSettings.userCollection();
         users.find({'assignments.projectId': projectIdParam},
                    { assignments: {$elemMatch: {projectId: projectIdParam}}})
-          .toArray(function(err, projectUsers){
+          .toArray(function(err, projectUsers) {
             if(err) {
                 res.status(400).json({error: 'Cannot find users'});
             } else {
                 res.json(projectUsers);
             }
         });
+    }
+};
+
+exports.restReplaceAssignments = function(req, res) {
+    var projectId = utils.getProjectId(req, res);
+    if(projectId) {
+        if(req.body) {
+            var user = req.body;
+            var userId = user._id;
+            var assignments = user.assignments;
+            var users = dbSettings.userCollection();
+            users.update({ _id: userId },
+                         { $pull: {assignments: {projectId: projectId} } },
+                         { multi: true },
+                function(err, result) {
+                    if(!err) {
+                        users.update({ _id: userId },
+                                     { $push: { assignments: { $each: assignments } }},
+                            function(err, updatedUser){
+                                console.log(updatedUser);
+                                // return saved object ????
+                            });
+                    }
+                });
+        } else {
+            res.status(500).json({error: 'Empty request body'});
+        }
     }
 };
 
