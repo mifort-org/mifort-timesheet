e0a093d278aeb3bb73569f4ab0dceec87805158e
altrofimov <a.d.trofimov@gmail.com>
2015-10-18 21:20:33 +0300

    Timelog styles

--- frontend/components/tabs/bootstrapTabs.js
+++ frontend/components/tabs/bootstrapTabs.js
@@ -21,8 +21,8 @@ angular.module('myApp')
         return {
             link: function (scope) {
                 scope.tabs = [
-                    {title: 'Timesheet'},
                     {title: 'Timelog'},
+                    {title: 'Timesheet'},
                     {title: 'Projects'},
                     {title: 'Report'},
                     {title: 'Company'},
