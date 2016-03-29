0accc4e6d10e387e239ab4f219b0f73d41427e07
altrofimov <a.d.trofimov@gmail.com>
2016-03-29 16:18:45 +0300

    Date Range picker issue fix #143

--- frontend/bower_components/bootstrap-daterangepicker/daterangepicker.js
+++ frontend/bower_components/bootstrap-daterangepicker/daterangepicker.js
@@ -52,11 +52,11 @@
                   '<div class="range_inputs">' +
                     '<div class="daterangepicker_start_input">' +
                       '<label for="daterangepicker_start"></label>' +
-                      '<input class="input-mini" type="text" name="daterangepicker_start" value="" />' +
+                      '<input class="input-mini" type="text" readonly name="daterangepicker_start" value="" />' +
                     '</div>' +
                     '<div class="daterangepicker_end_input">' +
                       '<label for="daterangepicker_end"></label>' +
-                      '<input class="input-mini" type="text" name="daterangepicker_end" value="" />' +
+                      '<input class="input-mini" type="text" readonly name="daterangepicker_end" value="" />' +
                     '</div>' +
                     '<button class="applyBtn" disabled="disabled"></button>&nbsp;' +
                     '<button class="cancelBtn"></button>' +
